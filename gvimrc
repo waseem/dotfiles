ab38b0b8d24d81a447d8ed8fc49f67b718335a61
Waseem Ahmad <talk.to.waseem@gmail.com>
2011-09-11 12:36:50 +0530

    * dmenu_run in xmonad * Battery display tweaks for xmobar * Hide menubar, right scrollbar and toolbar from gvim.

--- gvimrc
+++ gvimrc
@@ -1,1 +1,4 @@
 colorscheme railscasts
+set go-=r " Remove right scroll bar
+set go-=T " Remove toolbar
+set go-=m " Remote menu bar
--- xmonad.hs
+++ xmonad.hs
@@ -31,13 +31,20 @@ main = do
     } `additionalKeys`
     [
       ((mod1Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock") -- Lock screen
+    , ((mod1Mask, xK_p),               spawn myDmenu)
     , ((0, xK_Print),                  spawn "scrot") -- Screengrab of whole desktop. Saved to ~/
     ]
 
 -- Colors
+colorBlack   = "#000000"
 colorGrey    = "#484848"
 colorBlue    = "#60a0c0"
+colorBlueAlt = "#007b8c"
 colorRed     = "#d74b73"
+colorMagenta = "#8e82a2"
+
+-- Dmenu
+myDmenu = "dmenu_run -b -l 3 -p 'run' -nb '" ++ colorBlack ++ "' -nf '" ++ colorBlue ++ "' -sb '" ++ colorBlueAlt ++ "'"
 
 -- Manage floating windows
 myManageHook = composeAll [
