992bd30f847bfe929280be8c2af3eb1f4d64b971
Waseem Ahmad <talk.to.waseem@gmail.com>
2018-10-23 20:29:55 +0530

    Use i3.

--- bashrc
+++ bashrc
@@ -6,8 +6,10 @@
 [[ $- != *i* ]] && return
 
 [[ -f ~/.bash_aliases ]] && . ~/.bash_aliases
+[[ -f ~/.git-prompt.sh ]] && . ~/.git-prompt.sh
 
 PS1='\e[1;35m\u@\h\e[m: \e[1;34m\w\e[m\e[0;32m$(__git_ps1)\e[m:\n\$ '
 
-
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+[[ -f /usr/share/chruby/chruby.sh ]] && . /usr/share/chruby/chruby.sh
+[[ -f /usr/share/chruby/auto.sh ]] && . /usr/share/chruby/auto.sh
+eval "$(direnv hook bash)"
--- dualhead.sh
+++ dualhead.sh
@@ -2,18 +2,22 @@
 
 # Source: http://www.jejik.com/articles/2008/10/setting_up_dual_monitors_system-wide_with_xrandr_on_debian_lenny/
 
-xrandr | grep VGA1 | grep " connected "
+# xrandr | grep VGA1 | grep " connected "
+xrandr | grep HDMI2 | grep " connected "
 
 if [$? -eq 0]; then
   # External monitor is connected
-  xrandr --output VGA1 --auto --output LVDS1 --auto --left-of VGA1
+  # xrandr --output VGA1 --auto --output LVDS1 --auto --left-of VGA1
+  xrandr --output HDMI2 --auto --output eDP1 --auto --left-of HDMI2
 
   if [$? -ne 0]; then
     # Something went wrong. Autoconfigure the internal monitor and disable the external one
-    xrandr --output LVDS1 --auto --output VGA1 --off
+    # xrandr --output LVDS1 --auto --output VGA1 --off
+    xrandr --output eDP1 --auto --output HDMI2 --off
   fi
 
 else
   # External monitor is not connected
-  xrandr --output LVDS1 --auto --output VGA1 --off
+  # xrandr --output LVDS1 --auto --output VGA1 --off
+  xrandr --output eDP1 --auto --output HDMI2 --off
 fi
