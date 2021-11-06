800294f6fb2e4c5e2aa5e8a5c93b3353677d9b0e
Waseem Ahmad <waseem.ahmad@shopify.com>
2021-11-07 01:54:56 +0530

    Add plugins to nvim prepare setup.sh testing.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash 
 
+set -eu
+
+install_package() {
+  sudo apt-get install -y "$1"
+}
+
+setup_nvim() {
+  mkdir -p "$HOME/.config/nvim"
+}
+
 if [ $SPIN ]; then
+  link_source="$HOME/dotfiles"
   git config --global --unset-all credential.helper
-  echo "hello"
 fi
